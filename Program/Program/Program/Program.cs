fa9393c433a66fe279517224d62e0a29060bed5e
Alexander <Alexander@LOLKEKCHEBYREK>
2024-01-25 17:23:49 +0300

    Добавил комментарии

--- Program/Program/Program/Program.cs
+++ Program/Program/Program/Program.cs
@@ -4,22 +4,30 @@
     {
         static void Main(string[] args)
         {
+            //создаём новый экземпляр класса Functions
             var functions = new Functions();
 
+            //Предлагаем пользователю выбрать номер массива
             var arrayNumber = functions.ReadNumber();
 
+            //Производим валидацию ввода
             var validateInput = functions.ValidateInput(arrayNumber);
 
+            //Если введен некорректный номер массива, то предлагаем ввести его снова.
             while(!validateInput)
             {
                 arrayNumber = functions.ReadNumber();
                 validateInput = functions.ValidateInput(arrayNumber);
             }
 
+            //выбираем соответствующий масмив для дальнейшей обработки
             var workingArray = functions.GetArrayByNumber(arrayNumber);
 
+            //обрабатываем массив в соответствии с условием задачи
             var result = functions.ProcessArray(workingArray);
 
+
+            //отображаем полученный результат
             Console.WriteLine("Исходный массив:");
             functions.PrintStringArray(workingArray);
             Console.WriteLine("Обработанный массив:");
