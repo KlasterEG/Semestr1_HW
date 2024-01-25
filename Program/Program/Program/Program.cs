40f30c8e67460568e50e556d854e42977357654f
Alexander <Alexander@LOLKEKCHEBYREK>
2024-01-25 17:19:04 +0300

    Программа на C# для решения задачи

---
+++ Program/Program/Program/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var functions = new Functions();
+
+            var arrayNumber = functions.ReadNumber();
+
+            var validateInput = functions.ValidateInput(arrayNumber);
+
+            while(!validateInput)
+            {
+                arrayNumber = functions.ReadNumber();
+                validateInput = functions.ValidateInput(arrayNumber);
+            }
+
+            var workingArray = functions.GetArrayByNumber(arrayNumber);
+
+            var result = functions.ProcessArray(workingArray);
+
+            Console.WriteLine("Исходный массив:");
+            functions.PrintStringArray(workingArray);
+            Console.WriteLine("Обработанный массив:");
+            functions.PrintStringArray(result);
+
+        }
+    }
+}
